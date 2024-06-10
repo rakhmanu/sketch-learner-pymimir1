1f57e654871a1781978fd6644ad0a952be67ccc7
Ulzhalgas <11dulzhalgasr@gmail.com>
2024-06-10 14:38:32 +0000

    slurm

--- learn_all_2.bash
+++ learn_all_2.bash
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --gpus-per-node=0
+#SBATCH --gpus-per-node=1
 #SBATCH --mem-per-cpu=16g
 #SBATCH --partition=rleap_gpu_24gb
 #SBATCH --output=/work/rleap1/ulzhalgas.rakhman/%A.txt
