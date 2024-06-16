1bcf70bf85e8f5791e42a6b807fd9c4b483f953e
Ulzhalgas <11dulzhalgasr@gmail.com>
2024-06-16 17:30:21 +0000

    slurm

--- block_4_on.bash
+++ block_4_on.bash
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-node=1
-#SBATCH --mem-per-cpu=16g
+#SBATCH --mem-per-cpu=64g
 #SBATCH --partition=rleap_gpu_24gb
 #SBATCH --output=/work/rleap1/ulzhalgas.rakhman/%A.txt
 
