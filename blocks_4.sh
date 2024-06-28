b7319948619247d27059bf90a2bbbfa26a4dc2e0
Ulzhalgas Rakhman <ulzhalgas.rakhman@i6.informatik.rwth-aachen.de>
2024-06-28 16:42:26 +0200

    cluster

---
+++ blocks_4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-cpu=64g
+#SBATCH --partition=rleap_gpu_24gb
+#SBATCH --output=/work/rleap1/ulzhalgas.rakhman/%A.txt
+
+python3 learning/main.py --domain_filepath learning/benchmarks/blocks_4/domain.pddl --problems_directory learning/benchmarks/blocks_4/instances --width 1 --workspace workspace
